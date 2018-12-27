3526a8266d143c7dc7acfcb906698a704264d732
GuaiYiHu <guaiyihu@foxmail.com>
2018-12-27 16:11:45 +0800

    whyred: sepolicy: Address denials
    Change-Id: I5a859fe0d6f149d00570acb4f604429e662490a0

--- device.mk
+++ device.mk
@@ -142,7 +142,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
-    Snap
 
 # CNE
 PRODUCT_PACKAGES += \
